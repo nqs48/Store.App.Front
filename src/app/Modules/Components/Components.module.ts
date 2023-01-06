f70fc1f779fafd94e0b3b784d9ac6304246ecf6e
Nestea Turner <nqs48@icloud.com>
2023-01-06 14:55:36 -0500

    Adding components module

---
+++ src/app/Modules/Components/Components.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [NavBarComponent, FooterComponent],
+  exports: [NavBarComponent, FooterComponent]
+})
+export class ComponentsModule {}
