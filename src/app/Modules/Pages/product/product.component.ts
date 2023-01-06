4fdadfaa17f9635b5e01f20e8cbd78fcbcf1606c
Nestea Turner <nqs48@icloud.com>
2023-01-06 18:52:29 -0500

    Adding method get to component product

--- src/app/Modules/Pages/product/product.component.ts
+++ src/app/Modules/Pages/product/product.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProductService } from '../../Services/product/product.service';
+
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
+  products: any;
 
-  constructor() { }
+  constructor(
+    private router$: Router,
+    private productService$: ProductService
+  ) {}
 
   ngOnInit() {
+    this.productService$.getAllProducts().subscribe({
+      next: (data) => {
+        console.log('Return data products--: ', data);
+        this.products = data;
+      },
+      error: (err) => console.log(err),
+      complete: () => console.log('complete'),
+    });
   }
-
 }
