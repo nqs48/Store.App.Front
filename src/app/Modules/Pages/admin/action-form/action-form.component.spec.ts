9f27887403495094965451eebf363c68453ddc3d
Nestea Turner <nqs48@icloud.com>
2023-01-07 02:54:06 -0500

    Adding action-form component

---
+++ src/app/Modules/Pages/admin/action-form/action-form.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { ActionFormComponent } from './action-form.component';
+
+describe('ActionFormComponent', () => {
+  let component: ActionFormComponent;
+  let fixture: ComponentFixture<ActionFormComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ActionFormComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ActionFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
