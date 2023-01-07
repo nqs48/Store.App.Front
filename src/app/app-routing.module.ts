bda064cf3b048322a45671c9e75190c035ae105f
Nestea Turner <nqs48@icloud.com>
2023-01-07 02:17:26 -0500

    Updating routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 //Components
 import { HomeComponent } from './Modules/Pages/home/home.component';
 import { ProductComponent } from './Modules/Pages/product/product.component';
+import { AdminComponent } from './Modules/Pages/admin/admin.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'products', component: ProductComponent },
+  { path: 'addProduct', component: AdminComponent },
 ];
 
 @NgModule({
