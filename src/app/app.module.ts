fbf15d7f8b744766ea21a68c7e937396b43f940d
Nestea Turner <nqs48@icloud.com>
2023-01-07 02:18:27 -0500

    Adding Reactive forms to main module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,15 +11,18 @@ import { ComponentsModule } from './Modules/Components/Components.module';
 import { HomeComponent } from './Modules/Pages/home/home.component';
 import { ProductComponent } from './Modules/Pages/product/product.component';
 import { HttpClientModule } from '@angular/common/http';
+import { AdminComponent } from './Modules/Pages/admin/admin.component';
 
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, ProductComponent],
+  declarations: [AppComponent, HomeComponent, ProductComponent, AdminComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
     ComponentsModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
