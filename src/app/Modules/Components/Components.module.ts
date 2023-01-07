1107efd8a91e49061e511f16794d04ffc85ba77f
Nestea Turner <nqs48@icloud.com>
2023-01-07 02:55:15 -0500

    Implementing changes in section services

--- src/app/Modules/Components/Components.module.ts
+++ src/app/Modules/Components/Components.module.ts
@@ -3,10 +3,21 @@ import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FooterComponent } from './footer/footer.component';
 import { SectionServicesComponent } from './section-services/section-services.component';
+import { LoadingComponent } from './loading/loading.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [NavBarComponent, FooterComponent, SectionServicesComponent],
-  exports: [NavBarComponent, FooterComponent, SectionServicesComponent],
+  declarations: [
+    NavBarComponent,
+    FooterComponent,
+    SectionServicesComponent,
+    LoadingComponent,
+  ],
+  exports: [
+    NavBarComponent,
+    FooterComponent,
+    SectionServicesComponent,
+    LoadingComponent
+  ],
 })
 export class ComponentsModule {}
